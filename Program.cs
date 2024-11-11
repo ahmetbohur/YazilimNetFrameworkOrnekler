1910109ff8f300529cd6561c1dfb89cb8114270e
Ahmet Bohur <ahmetbohur@icloud.com>
2024-11-11 19:43:02 +0300

    SQLite Example Add

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using YazilimNetFrameworkOrnekler.Ornekler;
+using YazilimNetFrameworkOrnekler.VeriTabanlari.SQLite;
 
 namespace YazilimNetFrameworkOrnekler
 {
@@ -11,28 +12,13 @@ namespace YazilimNetFrameworkOrnekler
     {
         static void Main(string[] args)
         {
-            var result = TwoSum(new int[] { 2, 11, 15 ,  7}, 9);
-            Console.WriteLine(result[0] + " " + result[1]); 
+           SQLiteExample sQLiteExample = new SQLiteExample();
+            sQLiteExample.CheckConnectionDatabase();
+            sQLiteExample.CreateTable();
+            sQLiteExample.CreateAccount("Urhoba", "Ahmet", "Bohur", "123Password123321");
+            sQLiteExample.CreateAccount("Urhobaa", "Eren", "Bohur", "32123123asdasd");
             Console.ReadLine();
         }
-        public static int[] TwoSum(int[] nums, int target)
-        {
-            Dictionary<int, int> numMap = new Dictionary<int, int>();
-
-            for (int i = 0; i < nums.Length; i++)
-            {
-                int complement = target - nums[i];
-
-                if (numMap.ContainsKey(complement))
-                {
-                    return new int[] { numMap[complement], i };
-                }
-
-                numMap[nums[i]] = i;
-            }
-
-            throw new ArgumentException("No two sum solution");
-        }        
 
     }
 }
---
+++ VeriTabanlari/SQLite/SQLiteExample.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+
+namespace YazilimNetFrameworkOrnekler.VeriTabanlari.SQLite
+{
+    internal class SQLiteExample
+    {
+        private string databaseName = "ExampleSQLiteDatabase.sqlite";
+        private string connectionString;
+
+        internal SQLiteExample()
+        {
+            connectionString = $"Data Source={databaseName};Version=3;";
+            CreateDatabase();
+        }
+
+
+        private void CreateDatabase()
+        {
+            try
+            {
+                SQLiteConnection.CreateFile(databaseName);
+                Console.WriteLine("Veri Tabanı Başarı ile Oluşturuldu.");
+            } catch (Exception e)
+            {
+                Console.WriteLine("Veri Tabanı Oluşturulamadı!");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Veri Tabanı Oluşturulamadı!");
+            }
+        }
+
+
+        internal void CheckConnectionDatabase()
+        {
+            try
+            {
+                SQLiteConnection connection = new SQLiteConnection(connectionString);
+                connection.Open();
+                Console.WriteLine("SQLite veritabanına başarıyla bağlanıldı.");
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Veri Tabanı Bağlantısı Kurulamadı!");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Veri Tabanı Bağlantısı Kurulamadı!");
+            }
+        }
+
+        internal void CreateTable()
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string createTableQuery = "CREATE TABLE IF NOT EXISTS Accounts (Id INTEGER PRIMARY KEY, Username TEXT, Name TEXT, Surname TEXT, Password TEXT)";
+                    using (SQLiteCommand command = new SQLiteCommand(createTableQuery, connection))
+                    {
+                        command.ExecuteNonQuery();
+
+                        Console.WriteLine("Tablo Başarıyla Oluşturuldu.");
+                    }
+                }
+            } catch (Exception e)
+            {
+                Console.WriteLine("Tablo Oluşturulamadı!");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Tablo Oluşturulamadı!");
+            }
+        }
+
+        internal void CreateAccount(string username, string name, string surname, string password)
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO Accounts (Username, Name, Surname, Password) VALUES (@username, @name, @surname, @password)";
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@username", username);
+                        command.Parameters.AddWithValue("@name", name);
+                        command.Parameters.AddWithValue("@surname", surname);
+                        command.Parameters.AddWithValue("@password", password);
+                        command.ExecuteNonQuery();
+
+                        Console.WriteLine("Kullanıcı Başarıyla Eklendi.");
+                    }
+                }
+            } catch (Exception e)
+            {
+                Console.WriteLine("Hesap Oluşturulamadı!");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Hesap Oluşturulamadı!");
+            }
+        }
+          
+    }
+}
